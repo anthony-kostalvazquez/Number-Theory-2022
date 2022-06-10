064efbc5fd8a4300e16f0b9f84ed2d6026828cdf
anthony-kostalvazquez <47996134+anthony-kostalvazquez@users.noreply.github.com>
2022-06-10 12:48:25 -0600

    Add files via upload

---
+++ Discrete Log Solver In C/anthlib.h
@@ -0,0 +1,7 @@
+int brute_force_primality_test(int);
+
+int gmp_inverse(int, int);
+
+unsigned long int brute_force_discrete_log(unsigned long int, unsigned long int, unsigned long int);
+
+unsigned long int baby_step_giant_step_discrete_log (unsigned long int, unsigned long int, unsigned long int);
