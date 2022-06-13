05a3e654edefe5da073a6239ad2f9cbcea6831eb
anthony-kostalvazquez <47996134+anthony-kostalvazquez@users.noreply.github.com>
2022-06-13 16:17:20 -0600

    Delete anthlib.h

--- Discrete Log Solver In C/anthlib.h
+++
@@ -1,7 +0,0 @@
-int brute_force_primality_test(int);
-
-int gmp_inverse(int, int);
-
-unsigned long int brute_force_discrete_log(unsigned long int, unsigned long int, unsigned long int);
-
-unsigned long int baby_step_giant_step_discrete_log (unsigned long int, unsigned long int, unsigned long int);
